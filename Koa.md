ce284871922953e2529ec19a9f887fa9378a2ffb
duoluowang <309912361@qq.com>
2022-04-04 23:49:25 +0800

    新增模板

--- Koa.md
+++ Koa.md
@@ -38,3 +38,22 @@ npm install nodemon -D
 "start":"nodemon --exec babel-node app.js"
 ```
 
+## koa相关的包
+```
+npm install koa
+```
+
+## pm2
+```
+npm install pm2 -g
+pm2 ecosystem
+```
+- 修改ecosystem.config.js 把 script 选项改成 npm start
+- 修改 app.js 的模块引用方式， 把import 改成 require
+
+
+## 放弃本地修改，直接覆盖
+```
+1 git reset --hard
+2 git pull
+```
--- lesson-1.js
+++ lesson-1.js
@@ -1,9 +1,24 @@
 import Koa from 'koa'
+import Router from 'koa-router'
+import views from 'koa-views'
 
 const app = new Koa()
+const router = new Router()
 
-app.use(ctx => {
-  ctx.body = 'hello world'
+app.use(views('./views', {
+  map: {
+    ejs: 'ejs'
+  }
+}))
+
+router.get('/', async ctx => {
+  ctx.body = 'hello'
 })
+
+router.get('/nav', async ctx => {
+  await ctx.render('./index.ejs')
+})
+
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(3000)
